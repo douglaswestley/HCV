9efcfff5f799080cd8e3c18df98e124fcf74de56
Walicen <walicen.r@gmail.com>
2018-10-13 20:49:51 -0300

    Busca e salva os nomes encontrados no arquivo

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import csv
 
 usuarios = []
 
+def registra_ligacao(linha):
+    with open('ligacoes.csv','a') as f:
+        f.write(linha)
+    
+
 with open('usuarios.csv','r') as f:
     reader = csv.DictReader(f)
     for row in reader:
@@ -13,10 +18,7 @@ busca = input('Digite o nome do usuário.: ')
 if(busca):
     for element in usuarios:
         if(busca.upper() in element['DS_USUARIO'].upper()):
-            file = open('ligacoes.csv','w')
-            #print(f"{element['DS_USUARIO']} - {element['SETOR']}")
-
-
-
-
-
+            texto = f"\"{element['NM_USUARIO']}\",\"{element['DS_USUARIO']}\",\"{element['SETOR']}\"\n"
+            print(texto)
+            registra_ligacao(texto)
+            
