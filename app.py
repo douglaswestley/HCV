0dab874c753a0ea8afb0ef448bf20094b6f5ba11
Walicen <walicen.r@gmail.com>
2018-10-14 00:11:55 -0300

    Melhorando o script básico

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #Começando com Python
+#Versao Procedural / Funcional
 import csv
+from datetime import datetime as dt
 
 usuarios = []
 
@@ -12,13 +14,25 @@ with open('usuarios.csv','r') as f:
     reader = csv.DictReader(f)
     for row in reader:
         usuarios.append(row)
-
-busca = input('Digite o nome do usuário.: ')
-
-if(busca):
-    for element in usuarios:
-        if(busca.upper() in element['DS_USUARIO'].upper()):
-            texto = f"\"{element['NM_USUARIO']}\",\"{element['DS_USUARIO']}\",\"{element['SETOR']}\"\n"
-            print(texto)
+        
+while(True):        
+    busca = input('Digite o nome do usuário.: ')
+    result = []
+    cont=0
+    if(busca):
+    
+        for element in usuarios:
+            if(busca.upper() in element['DS_USUARIO'].upper()):
+                print("[{0}] - {1}".format(cont,element['DS_USUARIO']))
+                result.append(element)
+                cont += 1
+        if(len(result) > 0 ):
+            escolha = int(input("Digite o número referênte a sua escolha.: "))
+            print(f"Ligação registrada para {result[escolha]['DS_USUARIO']}")
+            texto = f"\"{result[escolha]['NM_USUARIO']}\",\"{result[escolha]['DS_USUARIO']}\",\"{result[escolha]['SETOR']}\",\"{dt.now().strftime('%d/%m/%Y %H:%M')}\"\n"
             registra_ligacao(texto)
+        else:
+            print('Não existe nenhum usuário com o nome digitado!')
+    
+            
             
